a17d719fe84fae17afa8b647bde5c93bd6837ef6
Benoit Brummer <trougnouf@gmail.com>
2016-02-06 00:55:11 -0500

    sync (starting Code/main/)

---
+++ Code/main/Common/main.h
@@ -0,0 +1,10 @@
+#ifndef MAINH
+#define MAINH
+// hw init/pinoutxxxx.h
+// common functions
+// UART: strOut
+// Laser: 3functions (include laser.h) (define BT, speed constants)
+// GPS: readDirFromSD, outputDir
+// BT: mp3toBT
+// 
+#endif
